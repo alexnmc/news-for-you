8b66554c0852609671ebfd50db1187b030bbe246
Alexander <nemechekalexander@gmail.com>
2019-07-04 17:01:44 +0300

    update

--- client/src/Button1.js
+++ client/src/Button1.js
@@ -11,13 +11,12 @@ import {withButton} from './ButtonProvider'
             props.loadingAnimation()
         }
     
-    
         function getNews2(url, name) {
             props.getNewsCountry(url, name)
             props.loadingAnimation()
         }
 
-       function  scrolling(){
+        function  scrolling(){
           props.editSwitch()
           myRef.current.scrollTo({top: 0, behavior: 'smooth'})
         }
@@ -85,7 +84,7 @@ import {withButton} from './ButtonProvider'
                     <p onClick = {()=> getNews2("gb", "United Kingdom")}>United Kingdom</p>
                     <p onClick = {()=> getNews2("us", "USA")}>USA</p>
                     <p onClick = {()=> getNews2("ve", "Venezuela")}>Venezuela</p>
-                    <p  className = 'switchTo' onClick = {() => scrolling()}>Channels</p>  
+                    <p  className = 'switchTo' onClick = {() => scrolling()}>switch to channels</p>  
                 </div>
                 :
                 <div>
@@ -121,7 +120,7 @@ import {withButton} from './ButtonProvider'
                     <p onClick = {()=> getNews("time", 'TIME')}>TIME</p>
                     <p onClick = {()=> getNews("usa-today", 'USA Today')}>USA Today</p>
                     <p onClick = {()=> getNews("the-wall-street-journal", 'Wall Street Journal')}>Wall Street Journal</p>
-                    <p className = 'switchTo' onClick = {() => scrolling()}>Countries</p>
+                    <p className = 'switchTo' onClick = {() => scrolling()}> switch to countries</p>
                 </div>
                 }
            </div>
